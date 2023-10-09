d394401344667faf798a601fb0de8b0b9a0c41d2
ico <francisco100eg@gmail.com>
2023-10-09 10:06:09 -0300

    Feat: Terminado navbar setup

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,24 +1,48 @@
-import Link from 'next/link'
-import Image from 'next/image'
+import Link from "next/link";
+import Image from "next/image";
+
+import { SlMagnifier } from "react-icons/sl";
+import { FaRegHeart } from "react-icons/fa";
+import { RxPerson } from "react-icons/rx";
+
+const navIcons = [
+  {
+    icon: <SlMagnifier />,
+    path: "/search",
+    alt: "search",
+  },
+  {
+    icon: <FaRegHeart />,
+    path: "/favorites",
+    alt: "favorites",
+  },
+  {
+    icon: <RxPerson />,
+    path: "/profile",
+    alt: "profile",
+  },
+];
+
 const Navbar = () => {
   return (
-    <header className='w-full'>
-      <nav className='nav'>
-        <Link href='/' className='flex items-center gap-1'>
+    <header className="w-full">
+      <nav className="nav">
+        <Link href="/" className="flex items-center gap-1">
+          <Image src="/assets/logo.png" alt="logo" width={35} height={35} />
 
-          <Image src='/assets/logo.png' alt='logo' width={35} height={35} />
-        
-        <p className="nav-logo">
-          Preço<span className='text-primary'>Mestre</span>
-        </p>
+          <p className="nav-logo">
+            Preço<span className="text-primary">Mestre</span>
+          </p>
+            </Link>
 
-        </Link>
-
-
-
+          <div className="flex items-center gap-5">
+            {navIcons.map((icon, index) => (
+              <Link key={`${icon.alt}-${index}`} href={icon.path}>{icon.icon}</Link>
+            ))}
+          </div>
       </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
