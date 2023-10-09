e72c2fb5ffc857821947c3755f8f6b6045296c6a
ico <francisco100eg@gmail.com>
2023-10-09 13:59:18 -0300

    Feat: Terminado carousel

--- components/HeroCarousel/HeroCarousel.tsx
+++ components/HeroCarousel/HeroCarousel.tsx
@@ -1,9 +1,52 @@
-import React from 'react'
+'use client'
+
+
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import Image from "next/image";
+const heroImages = [
+  {
+    imgUrl: "/assets/images/hero1.png",
+    alt: "Airfryer",
+  },
+  {
+    imgUrl: "/assets/images/hero3.png",
+    alt: "Smart TV",
+  },
+  {
+    imgUrl: "/assets/images/hero2.png",
+    alt: " SmartWatch",
+  },
+  {
+    imgUrl: "/assets/images/hero4.png",
+    alt: "Geladeira Smart",
+  },
+  {
+    imgUrl: "/assets/images/hero5.png",
+    alt: "Cadeira Escritório",
+  },
+];
 
 const HeroCarousel = () => {
   return (
-    <div>HeroCarousel</div>
-  )
-}
+    <div className="hero-carousel">
+      <Carousel showThumbs={false} autoPlay infiniteLoop interval={2000} showArrows={false} showStatus={false}>
+        {
+          heroImages.map((image) => (
+            <div className="w-[484px] h-[484px]">
+              <Image
+              src={image.imgUrl}
+              alt={image.alt}
+              fill
+              key={image.alt}
+              objectFit="contain"
+              />
+            </div>
+          ))
+        }
+      </Carousel>
+    </div>
+  );
+};
 
-export default HeroCarousel
+export default HeroCarousel;
