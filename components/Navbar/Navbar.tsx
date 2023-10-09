0144c667de7b2cc1b4483224b9e4772dcd294cc4
ico <francisco100eg@gmail.com>
2023-10-09 09:43:41 -0300

    Chore: continuação da criação da navbar e adicionado react-icons como dependencia

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
-
+import Link from 'next/link'
+import Image from 'next/image'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <header className='w-full'>
+      <nav className='nav'>
+        <Link href='/' className='flex items-center gap-1'>
+
+          <Image src='/assets/logo.png' alt='logo' width={35} height={35} />
+        
+        <p className="nav-logo">
+          Preço<span className='text-primary'>Mestre</span>
+        </p>
+
+        </Link>
+
+
+
+      </nav>
+    </header>
   )
 }
 
